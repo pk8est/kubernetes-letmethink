e8868f63caaf4cf088b39fa1a3240926f079a846
wuzhonggui <wuzhonggui@yy.com>
2019-02-23 13:26:37 +0800

    add

--- backend/module-web-service/src/main/java/com/pkest/web/service/k8s/impl/K8sNamespaceServiceImpl.java
+++ backend/module-web-service/src/main/java/com/pkest/web/service/k8s/impl/K8sNamespaceServiceImpl.java
@@ -1,29 +1,13 @@
 package com.pkest.web.service.k8s.impl;
 
-import com.fasterxml.jackson.databind.util.BeanUtil;
-import com.google.common.collect.ImmutableMap;
 import com.pkest.common.exception.HYException;
-import com.pkest.lib.kubernetes.KubeClient;
 import com.pkest.lib.kubernetes.exception.K8sDriverException;
-import com.pkest.repo.model.ClusterModel;
 import com.pkest.repo.model.NamespaceModel;
-import com.pkest.util.GsonUtils;
-import com.pkest.web.service.service.ClusterService;
-import com.pkest.web.service.warp.NamespaceWarp;
-import io.fabric8.kubernetes.api.model.IntOrString;
-import io.fabric8.kubernetes.api.model.Namespace;
 import com.pkest.web.service.k8s.K8sNamespaceService;
-import io.fabric8.kubernetes.api.model.NamespaceBuilder;
-import io.fabric8.kubernetes.api.model.apps.Deployment;
-import io.fabric8.kubernetes.api.model.apps.DeploymentBuilder;
-import io.fabric8.kubernetes.api.model.apps.DeploymentSpec;
-import io.fabric8.kubernetes.api.model.apps.DeploymentSpecBuilder;
+import com.pkest.web.service.warp.NamespaceWarp;
+import io.fabric8.kubernetes.api.model.Namespace;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author 360733598@qq.com
@@ -38,19 +22,10 @@ public class K8sNamespaceServiceImpl extends K8sBaseServiceImpl implements K8sNa
     }
 
     public Namespace build(NamespaceModel model, NamespaceWarp warp){
-
-        return new NamespaceBuilder()
-                .withNewMetadata()
-                .withName(model.getName())
-                .endMetadata()
-                .withNewSpec()
-                .withFinalizers("kubernetes")
-                .endSpec()
-                .build();
-       /* Namespace namespace = new Namespace();
+        Namespace namespace = new Namespace();
         BeanUtils.copyProperties(warp, namespace);
         warp.getMetadata().setName(model.getName());
-        return namespace;*/
+        return namespace;
     }
 
 }
